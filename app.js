539b2e6349ce947b0ce2d2d02aa5b159172f4d37
Roger Andretto Scheffeld <ro_scheffeld@magazineluiza.intranet>
2020-12-10 18:27:36 -0300

    fiz(app): add body-parser

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const routes = require('./src/routes/routes');
 
-app.use(routes)
+app.use(bodyParser.json());
+app.use(routes);
 
 module.exports = app
