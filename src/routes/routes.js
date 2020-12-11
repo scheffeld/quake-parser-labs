705ec281e1944dc401d776564ed47b07aabd60c7
Roger Andretto Scheffeld <ro_scheffeld@magazineluiza.intranet>
2020-12-11 16:38:54 -0300

    feat(routes): add find game
    - add route to find specifc game

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,4 +5,10 @@ routes.get('/api/v1/games', (req, res) => {
     return res.json(games)
 })
 
+routes.get('/api/v1/games/:game', (req, res) => {
+    const { game } = req.params
+    const findGame = `game_${game}`
+    return res.json({ [findGame]: games[findGame]})
+})
+
 module.exports = routes
