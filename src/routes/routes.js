de2e2b3e10d3855201e98d771d5f3c6ef272a07b
Roger Andretto Scheffeld <ro_scheffeld@magazineluiza.intranet>
2020-12-11 16:23:17 -0300

    feat(routes): add route response
    - returning all games on

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
+const games = require('../util/count');
 
-routes.get('/', (req, res) => {
-    return res.json({ server: "up" })
+routes.get('/api/v1/games', (req, res) => {
+    return res.json(games)
 })
 
 module.exports = routes
