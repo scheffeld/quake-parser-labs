70b394ba5666b9bf9a871397448f58cc0e0710a0
Roger <rscheffeld@gmail.com>
2020-12-11 22:39:38 -0300

    feat(tests): test suite
    - added unit test to read file
    
    - added unit test to parse file to JSON

---
+++ __tests__/unit/parser.spec.js
@@ -0,0 +1,10 @@
+const parser = require('../../src/util/parser');
+
+describe('Parse file to JSON', () => {
+    it('Should to parse a log file to JSON', () => {
+        const games = parser;
+
+        expect(games).toBeTruthy();
+        expect(typeof response).toBe('object');
+    });
+});
---
+++ __tests__/unit/readFile.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const readFile = require('../../src/util/readFile');
+
+describe('Read log file of the game', () => {
+    it('Should to read a log file', () => {
+        const file = readFile;
+
+        expect(file).toBeTruthy();
+        expect(typeof response).toBe('object');
+    });
+});
