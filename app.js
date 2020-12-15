b65612960be0088a3dc31c98f66ac8691b45d599
Roger Scheffeld <rscheffeld@Gmail.com>
2020-12-15 11:25:00 -0300

    fix(healthcheck): fix sintax of healtcheck route

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./src/routes/routes');
-const healthcheck = require('./src/routes/healthcheck.routes');
+const healthCheck = require('./src/routes/healthCheck.routes');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
-app.use(healthcheck);
+app.use(healthCheck);
 
 module.exports = app
