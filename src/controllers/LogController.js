a2607df822e226aea4c0162f288e418a9e8b7091
Roger Scheffeld <rscheffeld@Gmail.com>
2020-12-16 14:45:58 -0300

    feat(controllers): added game not found return

--- src/controllers/LogController.js
+++ src/controllers/LogController.js
@@ -1,13 +1,18 @@
 const games = require('../api/parser');
 
-module.exports = {
-  index(req, res) {
-    return res.status(200).json(games);
-  },
-  show(req, res) {
-    const { id } = req.params;
-    const key = `game_${id}`;
-
-    return res.status(200).json({ [key]: games[key] });
-  },
+const index = (req, res) => {
+  return res.status(200).json(games);
 };
+
+const show = (req, res) => {
+  const { id } = req.params;
+  const key = `game_${id}`;
+
+  if (!games[key]) {
+    return res.status(200).json({ [key]: 'Jogo não encontrado :(' });
+  }
+
+  return res.status(200).json({ [key]: games[key] });
+};
+
+module.exports = { index, show };
--- src/controllers/UploadFileController.js
+++ src/controllers/UploadFileController.js
@@ -1,12 +1,12 @@
-module.exports = {
-  upload(req, res) {
-    const { file } = req;
-    return res.status(201).json({
-      originalFileName: file.originalname,
-      newFileName: file.filename,
-      type: file.mimetype,
-      size: file.size,
-      upload: true,
-    });
-  },
+const upload = (req, res) => {
+  const { file } = req;
+  return res.status(201).json({
+    originalFileName: file.originalname,
+    newFileName: file.filename,
+    type: file.mimetype,
+    size: file.size,
+    upload: true,
+  });
 };
+
+module.exports = { upload };
