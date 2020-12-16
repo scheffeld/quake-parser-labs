68bcbfd7f0bce83227f9cacb63805e9ec665e74b
Roger Scheffeld <rscheffeld@Gmail.com>
2020-12-16 14:45:24 -0300

    feat(__tests__): added suit tests
    - added upload wrong file test
    
    - added upload correct file test
    
    - added game not found test

--- src/__tests__/controllers.test.js
+++ src/__tests__/controllers.test.js
@@ -1,5 +1,9 @@
 const request = require('supertest');
+const path = require('path');
 const app = require('../app');
+
+const correctFile = path.resolve(__dirname, '../data/games.log');
+const wrongFile = path.resolve(__dirname, '../../README.md');
 
 describe('Health Check', () => {
   it('Should returns application health', () => {
@@ -31,12 +35,44 @@ describe('List specific game', () => {
   });
 });
 
+describe('Game not fount', () => {
+  it('Should returned status http OK', async () => {
+    await request(app)
+      .get('/api/v1/games/23')
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+});
+
 describe('Upload without file', () => {
-  it('Should returned status http created', async () => {
+  it('Should returned status http internal server error', async () => {
     await request(app)
       .post('/api/v1/upload')
       .then((res) => {
         expect(res.status).toBe(500);
       });
   });
+});
+
+describe('Upload wrong type file', () => {
+  it('Shoul return status http internal server error', async () => {
+    await request(app)
+      .post('/api/v1/upload')
+      .attach('file', wrongFile)
+      .then((res) => {
+        expect(res.status).toBe(500);
+      });
+  });
+});
+
+describe('Upload file', () => {
+  it('Should return status http created', async () => {
+    await request(app)
+      .post('/api/v1/upload')
+      .attach('file', correctFile)
+      .then((res) => {
+        expect(res.status).toBe(201);
+      });
+  }, 5000);
 });
